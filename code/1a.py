dffa6c374621324c724112ff20776d57e4ade38b
MyNameIsTrez <welfje@gmail.com>
2021-12-01 14:06:05 +0100

    Change a and b to l and r

--- code/1a.py
+++ code/1a.py
@@ -4,4 +4,4 @@ import utils
 if __name__ == "__main__":
 	data = utils.parse("1", int)
 
-	print(sum([a > b for a, b in zip(data[1:], data)]))
+	print(sum([r > l for r, l in zip(data[1:], data)]))
--- code/1b.py
+++ code/1b.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
 	data = utils.parse("1", int)
 
 	windows = [sum(data[i:i+3]) for i in range(len(data))]
-	print(sum([a > b for a, b in zip(windows[1:], windows)]))
+	print(sum([r > l for r, l in zip(windows[1:], windows)]))
