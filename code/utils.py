a28d5fab50c680abc47a1afa29ae4a45d95a34e5
MyNameIsTrez <welfje@gmail.com>
2021-12-07 09:52:11 +0100

    Add parse_line_csv

--- code/utils.py
+++ code/utils.py
@@ -6,3 +6,8 @@ def parse(day, fun):
 def parse_line(day, fun):
 	with open("../inputs/" + day + ".txt") as f:
 		return fun(f.readline())
+
+
+def parse_line_csv(day):
+	with open("../inputs/" + day + ".txt") as f:
+		return list(map(int, f.readline().strip().split(",")))
