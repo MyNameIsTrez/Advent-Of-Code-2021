b60c9187028e57599feeb3ba8659b03062eded11
MyNameIsTrez <welfje@gmail.com>
2021-12-07 17:25:25 +0100

    Add 7a solution

--- code/7a.py
+++ code/7a.py
@@ -1,7 +1,7 @@
 import utils
 
 
-def get_diff_side(diff, side):
+def get_side_costs(diff, side):
 	diff_side = [0]
 
 	for i in range(len(diff)):
@@ -21,19 +21,38 @@ def get_diff_side(diff, side):
 	return diff_side
 
 
+def solution1(data):
+	data.sort() # TODO: It should be possible to get rid of this by letting get_side_costs() use abs()
+
+	diff = [l2_el - l1_el for l1_el, l2_el in zip(data[:-1], data[1:])]
+
+
+	left_costs = get_side_costs(diff, "left")
+	# print(left_costs)
+	right_costs = get_side_costs(diff, "right")
+	# print(right_costs)
+
+
+	scores = [l + r for l, r in zip(left_costs, right_costs)]
+	print(min(scores))
+
+
+# This uses the fact that for [1, 2, 10, 100] it costs an equal amount of fuel to go to 2/3/4.../9/10
+# [4, 5, 10, 100, 0, 0, 0, 0, 0]
+def solution2(data):
+	data.sort()
+
+	alignment_avg = data[int((len(data))/2)] # If there are 4 numbers this returns the 3rd number at index 2
+	# print(alignment_avg)
+
+	fuel = 0
+	for hor in data:
+		fuel += abs(alignment_avg - hor)
+	print(fuel)
+
+
 if __name__ == "__main__":
 	data = utils.parse_line_csv("7")
 
-	data.sort() # TODO: It should be possible to get rid of this by letting get_diff_side() use abs()
-
-	diff = [l2_el - l1_el for l1_el, l2_el in zip(data, data[1:])]
-
-
-	diff_left = get_diff_side(diff, "left")
-	# print(diff_left)
-	diff_right = get_diff_side(diff, "right")
-	# print(diff_right)
-
-
-	scores = [l + r for l, r in zip(diff_left, diff_right)]
-	print(min(scores))
+	# solution1(data.copy())
+	solution2(data.copy())
