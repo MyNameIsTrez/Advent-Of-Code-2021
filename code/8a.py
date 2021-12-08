444a986ff284463ccfc6e6a3b092459a94b82f41
MyNameIsTrez <welfje@gmail.com>
2021-12-08 10:16:31 +0100

    8a

---
+++ code/8a.py
@@ -0,0 +1,31 @@
+import utils
+
+
+def parse(line):
+	return line.strip().split(" | ")[1]
+
+
+def count_unique(line):
+	count = 0
+	for segment in line.split(" "):
+		# len 2 == digit 1
+		# len 4 == digit 4
+		# len 3 == digit 7
+		# len 7 == digit 8
+		if len(segment) in (2, 4, 3, 7):
+			count += 1
+	return count
+
+
+if __name__ == "__main__":
+	data = utils.parse("8", parse)
+
+	# print(data)
+
+	total_count = 0
+	for line in data:
+		count = count_unique(line)
+		total_count += count
+		print(line)
+		print(count)
+	print(total_count)
