39a7765fa48f98bfdc83733c4b993438b51ab52f
MyNameIsTrez <welfje@gmail.com>
2021-12-08 21:35:39 +0100

    Refactor digit_to_digit_segment_letters

--- code/8b.py
+++ code/8b.py
@@ -27,36 +27,7 @@ def get_sorted_segments(mixed_input_segments_list):
 	return unique_segments_list, non_unique_segments_list
 
 
-"""
- AAAA
-B    C
-B    C
- DDDD
-E    F
-E    F
- GGGG
-"""
-def get_potential_segment_letters_from_unique(unique_segments_list):
-	unique_segments_len_pair_digit = {
-		2: 1,
-		3: 7,
-		4: 4,
-		7: 8
-	}
-
-	digit_pair_digit_segment_letters = {
-		0: "ABCEFG",
-		1: "CF",      # unique
-		2: "ACDEG",
-		3: "ACDFG",
-		4: "BCDF",    # unique
-		5: "ABDFG",
-		6: "ABDEFG",
-		7: "ACF",     # unique
-		8: "ABCDEFG", # unique
-		9: "ABCDFG"
-	}
-
+def init_segment_letter_pair_potential_segment_letters():
 	segment_letter_pair_potential_segment_letters = {
 		'A': [],
 		'B': [],
@@ -71,21 +42,66 @@ def get_potential_segment_letters_from_unique(unique_segments_list):
 		for c in "abcdefg":
 			potential_segment_letters.append(c)
 
+	return segment_letter_pair_potential_segment_letters
+
+
+def unique_segments_len_pair_digit(unique_segments_len):
+	unique_segments_len_pair_digit = {
+		2: 1,
+		3: 7,
+		4: 4,
+		7: 8
+	}
+	return unique_segments_len_pair_digit[unique_segments_len]
+
+
+def digit_to_digit_segment_letters(digit):
+	digit_pair_digit_segment_letters = {
+		0: "ABCEFG",
+		1: "CF",      # unique
+		2: "ACDEG",
+		3: "ACDFG",
+		4: "BCDF",    # unique
+		5: "ABDFG",
+		6: "ABDEFG",
+		7: "ACF",     # unique
+		8: "ABCDEFG", # unique
+		9: "ABCDFG"
+	}
+	return digit_pair_digit_segment_letters[digit]
+
+
+def remove_non_unique_segments_from_potential_segment_letters(digit_segment_letters, segment_letter_pair_potential_segment_letters, unique_segments):
+	# TODO: Don't attempt to remove 8 as its letters are everywhere?
+	for letter in "ABCDEFG":
+		if letter not in digit_segment_letters:
+			potential_segment_letters = segment_letter_pair_potential_segment_letters[letter]
+
+			for potential_segment_letter in unique_segments:
+				if potential_segment_letter in potential_segment_letters:
+					potential_segment_letters.remove(potential_segment_letter)
+
+
+"""
+ AAAA
+B    C
+B    C
+ DDDD
+E    F
+E    F
+ GGGG
+"""
+def get_potential_segment_letters_from_unique(unique_segments_list):
+	segment_letter_pair_potential_segment_letters = init_segment_letter_pair_potential_segment_letters()
+
 	for unique_segments in unique_segments_list:
 		unique_segments_len = len(unique_segments)
 
-		digit = unique_segments_len_pair_digit[unique_segments_len]
+		digit = unique_segments_len_pair_digit(unique_segments_len)
 
-		digit_segment_letters = digit_pair_digit_segment_letters[digit]
+		digit_segment_letters = digit_to_digit_segment_letters(digit)
 
-		# TODO: Don't attempt to remove 8 as its letters are everywhere?
-		for letter in "ABCDEFG":
-			if letter not in digit_segment_letters:
-				potential_segment_letters = segment_letter_pair_potential_segment_letters[letter]
-
-				for potential_segment_letter in unique_segments:
-					if potential_segment_letter in potential_segment_letters:
-						potential_segment_letters.remove(potential_segment_letter)
+		remove_non_unique_segments_from_potential_segment_letters(digit_segment_letters, segment_letter_pair_potential_segment_letters, unique_segments)
 
 		# print(unique_segments, digit, digit_segment_letters, segment_letter_pair_potential_segment_letters, "\n")
 
