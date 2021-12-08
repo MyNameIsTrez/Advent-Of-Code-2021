97ae2f209a8ed737921a5b5637fff7757fafc71e
MyNameIsTrez <welfje@gmail.com>
2021-12-08 20:10:04 +0100

    Add 069 and 235 filters

--- code/8b.py
+++ code/8b.py
@@ -87,21 +87,113 @@ def get_potential_segment_letters_from_unique(unique_segments_list):
 	return segment_letter_pair_potential_segment_letters
 
 
-def remove_non_unique(potential_segment_letters, non_unique_segments_list):
-	pass
-	# remove_069(potential_segment_letters, non_unique_segments_list)
-	# remove_235(potential_segment_letters, non_unique_segments_list)
+def filter_non_unique(potential_segment_letters, non_unique_segments_list):
+	filter_069(potential_segment_letters, non_unique_segments_list)
+	filter_235(potential_segment_letters, non_unique_segments_list)
 
 
-def remove_069(potential_segment_letters, non_unique_segments_list):
-	segments_list_069 = filter_on_string_len(non_unique_segments_list, 6)
+"""
+   0:     6:      9:
+ AAAA    AAAA    AAAA
+B    C  B    .  B    C
+B    C  B    .  B    C
+ ....    DDDD    DDDD
+E    F  E    F  .    F
+E    F  E    F  .    F
+ GGGG    GGGG    GGGG
+"""
+def filter_069(potential_segment_letters, non_unique_segments_list):
+	len_069 = 6
+	segments_list_069 = filter_on_string_len(non_unique_segments_list, len_069)
 
-	for i in range():
-		for c in non_unique_segments:
-			pass
+	# print(segments_list_069)
 
-def remove_235(potential_segment_letters, non_unique_segments_list):
-	segments_list_235 = filter_on_string_len(non_unique_segments_list, 5)
+	for c in "abcdefg":
+		count = 0
+
+		for segment_069 in segments_list_069:
+			if c in segment_069:
+				count += 1
+
+		# print(count)
+
+		if count == 3: # c is either A / B / F / G, so remove c from C, D, E
+			if c in potential_segment_letters["C"]:
+				potential_segment_letters["C"].remove(c)
+			if c in potential_segment_letters["D"]:
+				potential_segment_letters["D"].remove(c)
+			if c in potential_segment_letters["E"]:
+				potential_segment_letters["E"].remove(c)
+
+		if count == 2: # c is either C / D / E, so remove c from A, B, F, G
+			if c in potential_segment_letters["A"]:
+				potential_segment_letters["A"].remove(c)
+			if c in potential_segment_letters["B"]:
+				potential_segment_letters["B"].remove(c)
+			if c in potential_segment_letters["F"]:
+				potential_segment_letters["F"].remove(c)
+			if c in potential_segment_letters["G"]:
+				potential_segment_letters["G"].remove(c)
+
+
+"""
+  2:      3:      5:
+ AAAA    AAAA    AAAA
+.    C  .    C  B    .
+.    C  .    C  B    .
+ DDDD    DDDD    DDDD
+E    .  .    F  .    F
+E    .  .    F  .    F
+ GGGG    GGGG    GGGG
+"""
+def filter_235(potential_segment_letters, non_unique_segments_list):
+	len_235 = 5
+	segments_list_235 = filter_on_string_len(non_unique_segments_list, len_235)
+
+	print(segments_list_235)
+
+	for c in "abcdefg":
+		count = 0
+
+		for segment_235 in segments_list_235:
+			if c in segment_235:
+				count += 1
+
+		# print(count)
+
+		if count == 3: # c is either A / D / G, so remove c from B, C, E, F
+			if c in potential_segment_letters["B"]:
+				potential_segment_letters["B"].remove(c)
+			if c in potential_segment_letters["C"]:
+				potential_segment_letters["C"].remove(c)
+			if c in potential_segment_letters["E"]:
+				potential_segment_letters["E"].remove(c)
+			if c in potential_segment_letters["F"]:
+				potential_segment_letters["F"].remove(c)
+
+		if count == 2: # c is either C / F, so remove c from A, B, D, E, G
+			if c in potential_segment_letters["A"]:
+				potential_segment_letters["A"].remove(c)
+			if c in potential_segment_letters["B"]:
+				potential_segment_letters["B"].remove(c)
+			if c in potential_segment_letters["D"]:
+				potential_segment_letters["D"].remove(c)
+			if c in potential_segment_letters["E"]:
+				potential_segment_letters["E"].remove(c)
+			if c in potential_segment_letters["G"]:
+				potential_segment_letters["G"].remove(c)
+
+		if count == 1: # c is either B / E, so remove c from A, C, D, F, G
+			if c in potential_segment_letters["A"]:
+				potential_segment_letters["A"].remove(c)
+			if c in potential_segment_letters["C"]:
+				potential_segment_letters["C"].remove(c)
+			if c in potential_segment_letters["D"]:
+				potential_segment_letters["D"].remove(c)
+			if c in potential_segment_letters["F"]:
+				potential_segment_letters["F"].remove(c)
+			if c in potential_segment_letters["G"]:
+				potential_segment_letters["G"].remove(c)
 
 
 def filter_on_string_len(lst, length):
@@ -118,13 +210,16 @@ if __name__ == "__main__":
 
 		unique_segments_list, non_unique_segments_list = get_sorted_segments()
 
+		# print(non_unique_segments_list)
+
 		potential_segment_letters = get_potential_segment_letters_from_unique(unique_segments_list)
 		# print(potential_segment_letters)
 
-		remove_non_unique(potential_segment_letters, non_unique_segments_list)
+		print(potential_segment_letters)
+
+		filter_non_unique(potential_segment_letters, non_unique_segments_list)
 
 		print(potential_segment_letters)
-		print(non_unique_segments_list)
 
 		break
 		# print(unsorted_mixed_segments)
