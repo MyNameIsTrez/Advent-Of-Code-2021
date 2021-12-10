9bebda529301defece12ece5621b6cb47f0d5985
MyNameIsTrez <welfje@gmail.com>
2021-12-10 18:33:01 +0100

    10a

---
+++ code/10a.py
@@ -0,0 +1,44 @@
+import utils
+
+
+def main():
+	data = utils.parse("10", parse)
+
+	# print(data)
+
+	pairs = {
+		"(": ")",
+		"[": "]",
+		"{": "}",
+		"<": ">"
+	}
+
+	points = {
+		")": 3,
+		"]": 57,
+		"}": 1197,
+		">": 25137
+	}
+
+	score = 0
+
+	for line in data:
+		stack = []
+
+		for char in line:
+			if char in pairs:
+				stack.append(char)
+			else:
+				if char != pairs[stack.pop()]:
+					score += points[char]
+					break
+
+	print(score)
+
+
+def parse(line):
+	return list(line[:-1])
+
+
+if __name__ == "__main__":
+	main()
