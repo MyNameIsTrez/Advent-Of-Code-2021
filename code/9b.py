1537468d7705cc1b5432a470a7b59b0ed6967df1
MyNameIsTrez <welfje@gmail.com>
2021-12-10 05:04:21 +0100

    Refactored 9b using points as dict keys

--- code/9b.py
+++ code/9b.py
@@ -3,22 +3,18 @@ from functools import reduce
 
 
 def main():
-	world = utils.parse("9", parse)
+	data = utils.parse(filename, parse)
 
-	# print(world)
+	world = { (y, x): height for y, row in enumerate(data) for x, height in enumerate(row) }
 
-	visited = [[False]*WIDTH for _ in range(HEIGHT)]
-
-	# print(visited)
+	visited = { (y, x): False for y in range(HEIGHT) for x in range(WIDTH) }
 
 	basin_sizes = []
 
-	for y in range(HEIGHT):
-		for x in range(WIDTH):
-			if is_low_point(world, x, y):
-				stack = [[x, y]]
-				basin_size = solve(world, visited, stack)
-				basin_sizes.append(basin_size)
+	for point in world:
+		if is_low_point(world, point):
+			basin_size = get_basin_size(world, visited, point)
+			basin_sizes.append(basin_size)
 
 	basin_sizes.sort()
 	three_largest_basins = basin_sizes[-3:]
@@ -29,48 +25,34 @@ def parse(line):
 	return list(map(int, list(line[:-1])))
 
 
-def is_low_point(world, x, y):
-	if y > 0 and world[y-1][x] <= world[y][x]: # Top edge
-		return False
-	if y + 1 < HEIGHT and world[y+1][x] <= world[y][x]: # Bottom edge
-		return False
-
-	if x > 0 and world[y][x-1] <= world[y][x]: # Left edge
-		return False
-	if x + 1 < WIDTH and world[y][x+1] <= world[y][x]: # Right edge
-		return False
-
+def is_low_point(world, point):
+	for neighbor in ( (point[0]-1, point[1]), (point[0]+1, point[1]), (point[0], point[1]-1), (point[0], point[1]+1) ):
+		if neighbor in world and world[neighbor] <= world[point]:
+			return False
 	return True
 
 
-def solve(world, visited, stack):
+def get_basin_size(world, visited, starting_point):
+	stack = [starting_point]
 	size = 0
 
 	while len(stack) > 0:
-		node = stack.pop()
-		x, y = node
-		# print(x, y)
+		point = stack.pop()
 
-		if not visited[y][x]:
-			visited[y][x] = True
+		if not visited[point] and world[point] < 9:
+			visited[point] = True
 
 			size += 1
-			# print(x, y)
 
-			if y > 0 and world[y-1][x] >= world[y][x] and world[y-1][x] < 9: # Top edge
-				stack.append([x, y - 1]) # Go up
-			if y + 1 < HEIGHT and world[y+1][x] >= world[y][x] and world[y+1][x] < 9: # Bottom edge
-				stack.append([x, y + 1]) # Go down
-
-			if x > 0 and world[y][x-1] >= world[y][x] and world[y][x-1] < 9: # Left edge
-				stack.append([x - 1, y]) # Go left
-			if x + 1 < WIDTH and world[y][x+1] >= world[y][x] and world[y][x+1] < 9: # Right edge
-				stack.append([x + 1, y]) # Go right
+			for neighbor in ( (point[0]-1, point[1]), (point[0]+1, point[1]), (point[0], point[1]-1), (point[0], point[1]+1) ):
+				if neighbor in world and world[neighbor] >= world[point]:
+					stack.append(neighbor)
 
 	return size
 
 
 if __name__ == "__main__":
+	filename = "9"
 	WIDTH = 100
 	HEIGHT = 100
 
