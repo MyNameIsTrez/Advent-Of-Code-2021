fb0ad3faa34ea7f2f41b3a50e01232637bb3545e
MyNameIsTrez <welfje@gmail.com>
2021-12-12 23:56:14 +0100

    Add parse function to 1a

--- code/1a.py
+++ code/1a.py
@@ -2,9 +2,13 @@ import utils
 
 
 def main():
-	data = utils.parse("1", int)
+	data = utils.parse("1", parse)
 
 	print(sum([r > l for r, l in zip(data[1:], data)]))
+
+
+def parse(line):
+	return int(line)
 
 
 if __name__ == "__main__":
