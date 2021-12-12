d38fe63bf14347228155b9f2c9b2040249fcc6b6
MyNameIsTrez <welfje@gmail.com>
2021-12-12 03:47:53 +0100

    Extra

--- code/1a.py
+++ code/1a.py
@@ -1,7 +1,11 @@
 import utils
 
 
-if __name__ == "__main__":
+def main():
 	data = utils.parse("1", int)
 
 	print(sum([r > l for r, l in zip(data[1:], data)]))
+
+
+if __name__ == "__main__":
+	main()
