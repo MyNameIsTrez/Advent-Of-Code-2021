d6364d55dccfe939b019283108a540ecdcfcde01
MyNameIsTrez <welfje@gmail.com>
2021-12-18 17:05:27 +0100

    16b

---
+++ code/16b.py
@@ -0,0 +1,176 @@
+import utils
+
+from math import prod
+
+from collections import deque
+
+from enum import Enum, auto
+
+class PacketType(Enum):
+	SUM = 0
+	PRODUCT = 1
+	MINIMUM = 2
+	MAXIMUM = 3
+	LITERAL_VALUE = 4
+	GREATER_THAN = 5
+	LESS_THAN = 6
+	EQUAL_TO = 7
+
+
+class LengthType(Enum):
+	SUBPACKETS_LENGTH = 0
+	SUBPACKET_COUNT = 1
+
+
+def main():
+	data = utils.parse_line('16', parse)
+
+	bits = get_hex_to_bits(data)
+
+	solution = solve(bits)
+	print(f"Solution: {solution}")
+
+
+def solve(bits):
+	global PACKET_VERSION_SUM
+
+	packet_version = get_packet_version(bits)
+	PACKET_VERSION_SUM += packet_version
+
+	packet_type = get_packet_type(bits)
+
+	match packet_type:
+		case PacketType.LITERAL_VALUE:
+			return get_literal_value(bits)
+		case _:
+			length_type = get_length_type(bits)
+
+			operands = []
+
+			match length_type:
+				case LengthType.SUBPACKETS_LENGTH:
+					subpackets_length = get_subpackets_length(bits)
+
+					starting_bits_length = len(bits)
+					while True:
+						new_bits_length = len(bits)
+						bits_read = starting_bits_length - new_bits_length
+
+						if bits_read == subpackets_length:
+							break
+
+						operand = solve(bits)
+						operands.append(operand)
+				case LengthType.SUBPACKET_COUNT:
+					subpacket_count = get_subpacket_count(bits)
+
+					for _ in range(subpacket_count):
+						operand = solve(bits)
+						operands.append(operand)
+
+			match packet_type:
+				case PacketType.SUM:
+					return sum(operands)
+				case PacketType.PRODUCT:
+					return prod(operands)
+				case PacketType.MINIMUM:
+					return min(operands)
+				case PacketType.MAXIMUM:
+					return max(operands)
+				case PacketType.GREATER_THAN:
+					return operands[0] > operands[1]
+				case PacketType.LESS_THAN:
+					return operands[0] < operands[1]
+				case PacketType.EQUAL_TO:
+					return operands[0] == operands[1]
+
+
+def parse(line):
+	return line.rstrip()
+
+
+def get_hex_to_bits(data):
+	bits_string_list = list(bin(int(data, 16))[2:])
+
+	sticking_out_length = len(bits_string_list) % 4
+
+	if sticking_out_length > 0:
+		padding_length = 4 - sticking_out_length
+		bits_string_list = padding_length * ["0"] + bits_string_list
+
+	bits_string_list = EXTRA_LEADING_PADDING_LENGTH * ["0"] + bits_string_list
+
+	return deque(map(int, bits_string_list))
+
+
+def get_packet_version(bits):
+	return popleft_n_bits_to_num(bits, 3)
+
+
+def popleft_n_bits(bits, n):
+	sub_bits = [str(bits.popleft()) for _ in range(n)]
+	return sub_bits
+
+
+def popleft_n_bits_to_num(bits, n):
+	sub_bits = popleft_n_bits(bits, n)
+	return sub_bits_to_num(sub_bits)
+
+
+def sub_bits_to_num(sub_bits):
+	return int("".join(sub_bits), 2)
+
+
+def get_packet_type(bits):
+	packet_type_num = popleft_n_bits_to_num(bits, 3)
+	packet_type = PacketType(packet_type_num)
+	return packet_type
+
+
+def get_literal_value(bits):
+	literal_bit_string = get_literal_bit_string(bits)
+
+	literal_value = sub_bits_to_num(literal_bit_string)
+	return literal_value
+
+
+def get_literal_bit_string(bits):
+	literal_bit_string = []
+
+	while True:
+		is_last_group_var = is_last_group(bits)
+
+		four_bits_string = popleft_n_bits(bits, 4)
+
+		literal_bit_string.extend(four_bits_string)
+
+		if is_last_group_var:
+			return literal_bit_string
+
+
+def is_last_group(bits):
+	return bits.popleft() == 0
+
+
+def get_length_type(bits):
+	length_type_id_bit = bits.popleft()
+	length_type = LengthType(length_type_id_bit)
+	return length_type
+
+
+def get_subpackets_length(bits):
+	return popleft_n_bits_to_num(bits, 15)
+
+
+def get_subpacket_count(bits):
+	return popleft_n_bits_to_num(bits, 11)
+
+
+if __name__ == '__main__':
+	EXTRA_LEADING_PADDING_LENGTH = 8
+
+	PACKET_VERSION_SUM = 0
+
+	main()
+
+	print(f"Packet version sum: {PACKET_VERSION_SUM}")
