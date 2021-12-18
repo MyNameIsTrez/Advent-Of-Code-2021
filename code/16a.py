d5a767f0509d2a9b2823ac023a5b4f621347a690
MyNameIsTrez <welfje@gmail.com>
2021-12-18 16:46:06 +0100

    16a

--- code/16a.py
+++ code/16a.py
@@ -6,50 +6,76 @@ from enum import Enum, auto
 
 class PacketType(Enum):
 	LITERAL_VALUE = 4
-	OPERATOR = 6
+	OPERATOR = auto()
+
+
+class LengthType(Enum):
+	SUBPACKETS_LENGTH = 0
+	SUBPACKET_COUNT = 1
 
 
 def main():
-	data = utils.parse_line('16_example_1', parse)
-	# print(data)
+	data = utils.parse_line('16', parse)
 
 	bits = get_hex_to_bits(data)
-	print(f"Bits without padding: {bits}")
 
-	pad_bits(bits, data)
-	print(f"Bits with padding: {bits}")
+	solve(bits)
+
+
+def solve(bits):
+	global PACKET_VERSION_SUM
 
 	packet_version = get_packet_version(bits)
-	print(f"Packet version: {packet_version}")
+	PACKET_VERSION_SUM += packet_version
 
 	packet_type = get_packet_type(bits)
-	print(f"Packet type: {packet_type}")
 
 	match packet_type:
 		case PacketType.LITERAL_VALUE:
-			parse_literal(bits)
+			return get_literal_value(bits)
+		case PacketType.OPERATOR:
+			length_type = get_length_type(bits)
+
+			operands = []
+
+			match length_type:
+				case LengthType.SUBPACKETS_LENGTH:
+					subpackets_length = get_subpackets_length(bits)
+
+					starting_bits_length = len(bits)
+					while True:
+						new_bits_length = len(bits)
+						bits_read = starting_bits_length - new_bits_length
+
+						if bits_read == subpackets_length:
+							break
+
+						operand = solve(bits)
+						operands.append(operand)
+				case LengthType.SUBPACKET_COUNT:
+					subpacket_count = get_subpacket_count(bits)
+
+					for _ in range(subpacket_count):
+						operand = solve(bits)
+						operands.append(operand)
 
 
 def parse(line):
 	return line.rstrip()
 
 
-def get_padded_data(data):
-	return padded_data
-
-
 def get_hex_to_bits(data):
-	bits_string = bin(int(data, 16))[2:]
-	return deque(map(int, bits_string))
+	bits_string_list = list(bin(int(data, 16))[2:])
 
+	sticking_out_length = len(bits_string_list) % 4
 
-def pad_bits(bits, data):
-	print(f"len(data): {len(data)}")
-	print(f"len(data) % 4: {len(data) % 4}")
-	padding = [0] * (len(data) % 4)
-	print(f"Padding: {padding}")
-	bits.extendleft(padding)
-	return bits
+	if sticking_out_length > 0:
+		padding_length = 4 - sticking_out_length
+		bits_string_list = padding_length * ["0"] + bits_string_list
+
+	bits_string_list = EXTRA_LEADING_PADDING_LENGTH * ["0"] + bits_string_list
+
+	return deque(map(int, bits_string_list))
 
 
 def get_packet_version(bits):
@@ -72,14 +98,15 @@ def sub_bits_to_num(sub_bits):
 
 def get_packet_type(bits):
 	packet_type_num = popleft_n_bits_to_num(bits, 3)
-	return PacketType(packet_type_num)
+	packet_type = PacketType.LITERAL_VALUE if packet_type_num == 4 else PacketType.OPERATOR
+	return packet_type
 
 
-def parse_literal(bits):
+def get_literal_value(bits):
 	literal_bit_string = get_literal_bit_string(bits)
 
-	value = sub_bits_to_num(literal_bit_string)
-	print(f"Literal value: {value}")
+	literal_value = sub_bits_to_num(literal_bit_string)
+	return literal_value
 
 
 def get_literal_bit_string(bits):
@@ -87,13 +114,10 @@ def get_literal_bit_string(bits):
 
 	while True:
 		is_last_group_var = is_last_group(bits)
-		# print(f"Is last group: {is_last_group_var}")
 
 		four_bits_string = popleft_n_bits(bits, 4)
-		# print(f"Four bits string: {four_bits_string}")
 
 		literal_bit_string.extend(four_bits_string)
-		# print(f"Literal bit string: {literal_bit_string}")
 
 		if is_last_group_var:
 			return literal_bit_string
@@ -103,5 +127,25 @@ def is_last_group(bits):
 	return bits.popleft() == 0
 
 
+def get_length_type(bits):
+	length_type_id_bit = bits.popleft()
+	length_type = LengthType(length_type_id_bit)
+	return length_type
+
+
+def get_subpackets_length(bits):
+	return popleft_n_bits_to_num(bits, 15)
+
+
+def get_subpacket_count(bits):
+	return popleft_n_bits_to_num(bits, 11)
+
+
 if __name__ == '__main__':
+	EXTRA_LEADING_PADDING_LENGTH = 8
+
+	PACKET_VERSION_SUM = 0
+
 	main()
+
+	print(f"Packet version sum: {PACKET_VERSION_SUM}")
