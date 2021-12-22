41cb807b1f6ebdd9a9c9c706b5284602bbfd6632
MyNameIsTrez <welfje@gmail.com>
2021-12-22 19:06:18 +0100

    Let parse() remove out-of-bounds coordinates

--- code/22a.py
+++ code/22a.py
@@ -2,26 +2,38 @@ import utils
 
 
 def main():
-	data = utils.parse("22_example_1", parse)
+	data = parse()
 
 	print(data)
 
 
-def parse(line):
-	state, coordinates = line.split()
+def parse():
+	data = []
 
-	x_coordinates, y_coordinates, z_coordinates = coordinates.split(",")
+	with open("../inputs/22.txt") as f:
+		for line in f.readlines():
+			state, coordinates = line.split()
 
-	x_start, x_end = get_start_and_end(x_coordinates)
-	y_start, y_end = get_start_and_end(y_coordinates)
-	z_start, z_end = get_start_and_end(z_coordinates)
+			x_coordinates, y_coordinates, z_coordinates = coordinates.split(",")
 
-	return {
-		"state": state,
-		"x_start": x_start, "x_end": x_end,
-		"y_start": y_start, "y_end": y_end,
-		"z_start": z_start, "z_end": z_end
-	}
+			x_start, x_end = get_start_and_end(x_coordinates)
+			y_start, y_end = get_start_and_end(y_coordinates)
+			z_start, z_end = get_start_and_end(z_coordinates)
+
+			if (x_start < MINIMUM_COORDINATE or x_end > MAXIMUM_COORDINATE or
+				y_start < MINIMUM_COORDINATE or y_end > MAXIMUM_COORDINATE or
+				z_start < MINIMUM_COORDINATE or z_end > MAXIMUM_COORDINATE
+				):
+				continue
+
+			data.append({
+				"state": state,
+				"x_start": x_start, "x_end": x_end,
+				"y_start": y_start, "y_end": y_end,
+				"z_start": z_start, "z_end": z_end
+			})
+
+	return data
 
 
 def get_start_and_end(coordinates):
@@ -32,4 +44,7 @@ def get_start_and_end(coordinates):
 
 
 if __name__ == "__main__":
+	MINIMUM_COORDINATE = -50
+	MAXIMUM_COORDINATE = 50
+
 	main()
