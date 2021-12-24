6381374f07fd770ca448656b2915438dc776a296
MyNameIsTrez <welfje@gmail.com>
2021-12-24 23:12:07 +0100

    24a instruction reader done

--- code/24a.py
+++ code/24a.py
@@ -2,9 +2,42 @@ import utils
 
 
 def main():
-	data = utils.parse("24_example", parse)
+	instructions = utils.parse("24_example_2", parse)
 
-	print(data)
+	state = {
+		"w": 0,
+		"x": 0,
+		"y": 0,
+		"z": 0,
+	}
+
+	digits = get_digits_from_int(13)
+
+	for instruction in instructions:
+		match instruction["type"]:
+			case "input":
+				input_variable = instruction["input_variable"]
+				most_significant_digit = digits.pop(0)
+				state[input_variable] = most_significant_digit
+			case "operation":
+				operator = instruction["operator"]
+				operand_left = instruction["operand_left"]
+				operand_right = instruction["operand_right"]
+				operand_right_value = state[operand_right] if type(operand_right) == str else operand_right
+				
+				match operator:
+					case "add":
+						state[operand_left] += operand_right_value
+					case "mul":
+						state[operand_left] *= operand_right_value
+					case "div":
+						state[operand_left] //= operand_right_value # TODO: Might need to round up towards zero when negative
+					case "mod":
+						state[operand_left] %= operand_right_value
+					case "eql":
+						state[operand_left] = 1 if state[operand_left] == operand_right_value else 0
+
+	print(state)
 
 
 def parse(line):
@@ -13,11 +46,11 @@ def parse(line):
 	if len(split) == 2:
 		return {
 			"type": "input",
-			"input": split[1],
+			"input_variable": split[1],
 		}
 	else:
 		return {
-			"type": "operator",
+			"type": "operation",
 			"operator": split[0],
 			"operand_left": attempt_convert_to_int(split[1]),
 			"operand_right": attempt_convert_to_int(split[2]),
@@ -26,6 +59,10 @@ def parse(line):
 
 def attempt_convert_to_int(string):
 	return int(string) if string.lstrip("-").isdecimal() else string
+
+
+def get_digits_from_int(integer):
+	return list(map(int, str(integer)))
 
 
 if __name__ == "__main__":
