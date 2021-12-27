f096701bb3c358f54e42881eaa18efb191cd63ca
MyNameIsTrez <welfje@gmail.com>
2021-12-27 22:41:33 +0100

    24a iterate model numbers down, instead of up, which prevents immediately stopping

--- code/24a.py
+++ code/24a.py
@@ -4,14 +4,15 @@ import utils
 def main():
 	instructions = utils.parse("24", parse)
 
-	minimum_model_number = get_minimum_model_number()
-	model_number = minimum_model_number
+	model_number = get_maximum_model_number()
 	# model_number = 13579246899999
 
-	while "0" in str(model_number) or is_valid_model_number(model_number, instructions):
-		model_number += 1
+	while "0" in str(model_number) or not is_valid_model_number(model_number, instructions):
+		model_number -= 1
+		if model_number % 10000 == 0:
+			print(model_number)
 	
-	print(f"The largest model number accepted by MONAD is {model_number - 1}")
+	print(f"The largest model number accepted by MONAD is {model_number}")
 
 
 def parse(line):
@@ -35,13 +36,13 @@ def attempt_convert_to_int(string):
 	return int(string) if string.lstrip("-").isdecimal() else string
 
 
-def get_minimum_model_number():
+def get_maximum_model_number():
 	"""
 	Submarine model numbers are always fourteen-digit numbers
 	consisting only of digits 1 through 9.
 	The digit 0 cannot appear in a model number.
 	"""
-	return int("1" * 14)
+	return int("9" * 14)
 
 
 def is_valid_model_number(model_number, instructions):
@@ -58,8 +59,8 @@ def is_valid_model_number(model_number, instructions):
 		match instruction["type"]:
 			case "input":
 				input_variable = instruction["input_variable"]
-				most_significant_digit = digits.pop(0)
-				state[input_variable] = most_significant_digit
+				next_most_significant_digit = digits.pop(0)
+				state[input_variable] = next_most_significant_digit
 			case "operation":
 				operator = instruction["operator"]
 				operand_left = instruction["operand_left"]
@@ -75,7 +76,7 @@ def is_valid_model_number(model_number, instructions):
 						state[operand_left] //= operand_right_value # TODO: Might need to round up towards zero when negative
 					case "mod":
 						state[operand_left] %= operand_right_value
-						# state[operand_left] = math.fmod(state[operand_left], operand_right_value)
+						# state[operand_left] = math.fmod(state[operand_left], operand_right_value) # TODO: Different behavior for negative values
 					case "eql":
 						state[operand_left] = 1 if state[operand_left] == operand_right_value else 0
 
